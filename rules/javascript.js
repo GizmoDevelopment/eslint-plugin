fee5b29a34f1c2125c69e541ec0390b957c0086f
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-07-09 16:45:45 +0200

    import rulesets

---
+++ rules/javascript.js
@@ -0,0 +1,37 @@
+module.exports = {
+	"extends": [
+		"eslint:recommended"
+	],
+	"rules": {
+
+		// Disabled rules
+		"eol-last": "off",
+		"no-tabs": "off",
+
+		// Enforced rules
+		"quotes": [ "error" ],
+		"semi": [ "error" ],
+		"indent": [ "error", "tab", {
+			"SwitchCase": 1,
+			"offsetTernaryExpressions": true
+		}],
+		"space-infix-ops": [ "error" ],
+		"camelcase": [ "error", {
+			"properties": "always"
+		}],
+		"brace-style": [ 2, "1tbs", {
+			"allowSingleLine": true
+		}],
+		"prefer-const": "error",
+		"eqeqeq": [ "error", "always" ],
+		"template-curly-spacing": [ "error", "never" ],
+		"space-before-function-paren": [ "error" ],
+		"padded-blocks": "off",
+		"array-bracket-spacing": [ "error", "always", {
+			"objectsInArrays": false,
+			"arraysInArrays": false
+		}],
+		"one-var": [ "error", "always" ],
+
+	}
+};
---
+++ rules/typescript.js
@@ -0,0 +1,87 @@
+module.exports = {
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended",
+		"./javascript.js"
+	],
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+
+		// Disabled rules
+		"eol-last": "off",
+		"no-tabs": "off",
+		"indent": "off",
+		"semi": "off",
+		"quotes": "off",
+		"space-infix-ops": "off",
+		"camelcase": "off",
+		"space-before-function-paren": "off",
+		"padded-blocks": "off",
+		"no-unused-vars": "off",
+
+		// TSLint replacement rules
+		"@typescript-eslint/indent": [ "error", "tab", {
+			"SwitchCase": 1,
+			"offsetTernaryExpressions": true
+		}],
+		"@typescript-eslint/semi": [ "error" ],
+		"@typescript-eslint/quotes": [ "error" ],
+		"@typescript-eslint/space-infix-ops": [ "error" ],
+		"@typescript-eslint/space-before-function-paren": [ "error" ],
+
+		// Enforced rules
+		"@typescript-eslint/naming-convention": [ "error",
+			{
+				"selector": [ "variableLike", "function" ],
+				"format": [ "camelCase", "UPPER_CASE" ],
+				"leadingUnderscore": "allow"
+			},
+			{
+				"selector": "memberLike",
+				"modifiers": [ "private" ],
+				"format": [ "camelCase" ],
+				"leadingUnderscore": "require"
+			},
+			{
+				"selector": "typeLike",
+				"format": [ "PascalCase" ]
+			}
+		],
+		"@typescript-eslint/member-delimiter-style": [ "error", {
+			"multiline": {
+				"delimiter": "semi"
+			},
+			"singleline": {
+				"delimiter": "semi"
+			}
+		}],
+		"@typescript-eslint/member-ordering": [ "error", {
+			"classExpressions": {
+				"order": "alphabetically"
+			}
+		}],
+
+		// Typing rules
+		"@typescript-eslint/consistent-type-imports": [ "error" ],
+		"@typescript-eslint/consistent-type-exports": [ "error" ],
+		"@typescript-eslint/type-annotation-spacing": [ "error", {
+			"overrides": {
+				"colon": {
+					"before": false,
+					"after": true
+				},
+				"arrow": {
+					"before": true,
+					"after": true
+				}
+			}
+		}],
+		"@typescript-eslint/explicit-function-return-type": [ "error", {
+			"allowExpressions": true
+		}]
+
+	}
+};
