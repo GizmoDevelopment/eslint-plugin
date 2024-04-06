7519abb574191167f82c96e85d8e7cba59db3102
Tjaz <tjaz@gizmo.moe>
2024-04-06 16:50:10 +0200

    nicer export style

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ import js from "./rules/javascript.js";
 import ts from "./rules/typescript.js";
 import vue from "./rules/vue.js";
 
-export default {
-	configs: {
-		js,
-		ts,
-		vue
-	}
+export const configs = {
+	js,
+	ts,
+	vue
 };
